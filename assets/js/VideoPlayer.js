9fa2df1c43b0b698f89c7120942c1f3a8ad54728
Studio Syro <yfndws@gmail.com>
2024-01-31 17:08:32 -0500

    Update VideoPlayer.js

--- assets/js/VideoPlayer.js
+++ assets/js/VideoPlayer.js
@@ -18,6 +18,31 @@ function OpenVideo() {
     // Set the volume to 50%
     video.volume = 0.5;
 
+   // Event listener when video data is loaded
+   video.addEventListener('loadeddata', function() {
+	console.log("Video data has loaded.");
+	});
+
+	// Event listener to check if the video can be played
+	video.addEventListener('canplay', function() {
+	console.log("Video can be played.");
+	});
+
+	// Error event listener
+	video.addEventListener('error', function(e) {
+	console.error("Error loading video:", e);
+	});
+
     // Play the video
     video.play();
+
+	if (playPromise !== undefined) {
+        playPromise.then(function() {
+            // Automatic playback started successfully
+        }).catch(function(error) {
+            console.error("Error trying to play the video: ", error);
+            // Automatic playback failed
+            // Show a UI element to let the user manually start playback
+        });
+    }
 }
